5c29ea9e416f29979533b6676d333da9c09a5b0d
Bartłomiej Zawalski <bz337736@students.mimuw.edu.pl>
2016-01-22 21:42:02 +0100

    removed bugs in color changing
    Signed-off-by: Bartłomiej Zawalski <bz337736@students.mimuw.edu.pl>

--- QChromosome/mediacontrol.cpp
+++ QChromosome/mediacontrol.cpp
@@ -2,9 +2,12 @@
 
 MediaControl::MediaControl(QWidget *parent) :
     QPushButton(parent),
+    __color(Qt::transparent),
+    __hoverColor(Qt::transparent),
     effect(new QGraphicsColorizeEffect(this))
 {
-    effect->setColor("#b3b3b3");
+    effect->setColor(Qt::transparent);
+
     setGraphicsEffect(effect);
 }
 
@@ -13,11 +16,38 @@ MediaControl::~MediaControl()
 
 }
 
+QColor MediaControl::color() const
+{
+    return __color;
+}
+
+void MediaControl::setColor(const QColor &c)
+{
+    effect->setColor(__color = c);
+}
+
+QColor MediaControl::hoverColor() const
+{
+    return __hoverColor;
+}
+
+void MediaControl::setHoverColor(const QColor &c)
+{
+    __hoverColor = c;
+}
+
+void MediaControl::click()
+{
+    QPushButton::click();
+
+    effect->setColor(color());
+}
+
 void MediaControl::enterEvent(QEvent *event)
 {
     QPushButton::enterEvent(event);
 
-    effect->setColor("#ffffff");
+    effect->setColor(hoverColor());
 }
 
 void MediaControl::leaveEvent(QEvent *event)
@@ -25,7 +55,7 @@ void MediaControl::leaveEvent(QEvent *event)
     QPushButton::leaveEvent(event);
 
     if (!isChecked())
-        effect->setColor("#b3b3b3");
+        effect->setColor(color());
 }
 
 #include <QPainter>
@@ -33,5 +63,6 @@ void MediaControl::leaveEvent(QEvent *event)
 void MediaControl::paintEvent(QPaintEvent *)
 {
     QPainter painter(this);
+
     icon().paint(&painter, rect(), Qt::AlignCenter, QIcon::Normal, isChecked() ? QIcon::On : QIcon::Off);
 }
--- QChromosome/mediacontrol.h
+++ QChromosome/mediacontrol.h
@@ -7,10 +7,24 @@
 class MediaControl : public QPushButton
 {
     Q_OBJECT
+
+    Q_PROPERTY(QColor color READ color WRITE setColor)
+    Q_PROPERTY(QColor hoverColor READ hoverColor WRITE setHoverColor)
+
 public:
     explicit MediaControl(QWidget *parent = 0);
     virtual ~MediaControl();
 
+    QColor color() const;
+    void setColor(const QColor &c);
+
+    QColor hoverColor() const;
+    void setHoverColor(const QColor &c);
+    /* cf. QTBUG-2982 */
+
+    void click();
+
+protected:
     void enterEvent(QEvent*);
     void leaveEvent(QEvent*);
 
@@ -18,6 +32,9 @@ public:
 
 private:
     QGraphicsColorizeEffect *effect;
+
+    QColor __color;
+    QColor __hoverColor;
 };
 
 #endif // MEDIACONTROL_H
