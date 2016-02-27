c3b1bd20c0044d516a91df95eb05fd959cc6e793
Bartłomiej Zawalski <bz337736@students.mimuw.edu.pl>
2016-02-28 00:57:17 +0100

    Fixed bugs with icons in dockWidgets

--- QChromosome/dockwidget.cpp
+++ QChromosome/dockwidget.cpp
@@ -1,6 +1,6 @@
 #include "dockwidget.h"
 #include "ui_dockwidget.h"
-
+#include <QDebug>
 DockWidget::DockWidget(QWidget *parent) :
     QDockWidget(parent),
     ui(new Ui::DockWidget)
@@ -9,9 +9,28 @@ DockWidget::DockWidget(QWidget *parent) :
     ui->setupUi(a);
     setTitleBarWidget(a);
 
+    connect(this, &QDockWidget::windowTitleChanged, ui->label, &QLabel::setText);
+
     connect(ui->floatButton, &QPushButton::toggled, this, &QDockWidget::setFloating);
     connect(ui->closeButton, &QPushButton::clicked, this, &QDockWidget::close);
-    //connect(this, &QDockWidget::topLevelChanged, ui->floatButton, &QPushButton::setChecked);
+
+    connect(this, &QDockWidget::topLevelChanged, ui->floatButton, [this](bool x) {
+        if (x != this->ui->floatButton->isChecked())
+        {
+        ui->floatButton->disconnect();
+        ui->floatButton->toggle();
+        connect(ui->floatButton, &QPushButton::toggled, this, &QDockWidget::setFloating);
+        }
+
+        //wyślij zdarzenie hoverleave, jeśli trzeba
+
+        if (!x)
+        {
+            QEvent event(QEvent::HoverLeave);
+QApplication::sendEvent(ui->floatButton, &event);
+        }
+    }
+        );
 }
 
 DockWidget::~DockWidget()
