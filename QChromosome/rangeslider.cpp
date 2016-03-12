7b95e13b727a4a4a27c91a9e7a8383f9056b4af3
Bartłomiej Zawalski <bz337736@students.mimuw.edu.pl>
2016-03-12 16:20:04 +0100

    Improved implementation of paintEvent

--- QChromosome/rangeslider.cpp
+++ QChromosome/rangeslider.cpp
@@ -41,8 +41,8 @@ void RangeSlider::mouseMoveEvent(QMouseEvent *event)
 
     if (range.first > range.second)
         std::swap(*handle, value);
-
-    update();
+    else
+        update();
 }
 
 void RangeSlider::mouseReleaseEvent(QMouseEvent *event)
@@ -68,40 +68,33 @@ void RangeSlider::paintEvent(QPaintEvent *event)
 
     p.drawRect(10, -1, width() - 20, 2);
 
-    p.setBrush(QColor("#0055d4"));
+    int lowerBoundPosition = QStyle::sliderPositionFromValue(minimum(), maximum(), range.first, width() - 20) + 10;
+    int upperBoundPosition = QStyle::sliderPositionFromValue(minimum(), maximum(), range.second, width() - 20) + 10;
 
-    p.drawRect(QStyle::sliderPositionFromValue(minimum(), maximum(), range.first, width() - 20) + 10, -1, QStyle::sliderPositionFromValue(minimum(), maximum(), range.second - range.first, width() - 20), 2);
+    int r;
 
-    p.setRenderHint(QPainter::Antialiasing);
+    if (range.first != range.second)
+    {
+        p.setBrush(QColor("#0055d4"));
 
-    if (range.first==range.second)
+        p.drawRect(lowerBoundPosition, -1, upperBoundPosition - lowerBoundPosition, 2);
+
+        p.setRenderHint(QPainter::Antialiasing);
+
+        r = handle == &range.first ? 9 : 6;
+        p.drawEllipse(QPoint(lowerBoundPosition, 0), r, r);
+
+        r = handle == &range.second ? 9 : 6;
+        p.drawEllipse(QPoint(upperBoundPosition, 0), r, r);
+    }
+    else
     {
         p.setPen(QPen(QColor("#666666"), 2));
         p.setBrush(QColor("#1a1a1a"));
+
+        p.setRenderHint(QPainter::Antialiasing);
+
+        r = handle ? 9 : 6;
+        p.drawEllipse(QPoint(lowerBoundPosition, 0), r, r);
     }
-
-    p.drawEllipse(QPoint(QStyle::sliderPositionFromValue(minimum(), maximum(), range.first, width() - 20) + 10, 0), handle==&range.first||(range.first==range.second&&handle) ? 9 : 6,  handle==&range.first||(range.first==range.second&&handle) ? 9 : 6);
-
-    if (range.first!=range.second)
-    p.drawEllipse(QPoint(QStyle::sliderPositionFromValue(minimum(), maximum(), range.second, width() - 20) + 10, 0), handle==&range.second ? 9 : 6,  handle==&range.second ? 9 : 6);
-
-    /*if (handle){
-        p.setRenderHint(QPainter::Antialiasing, false);
-        p.translate(10+*handle*(width()-20)/(maximum()-minimum()),0);
-
-
-
-        p.setPen(QColor("#767676"));
-        p.setBrush(QColor("#2b2b2b"));
-        p.drawRect(QRect(-30, -30, 60, 20));
-
-
-   p.setRenderHint(QPainter::Antialiasing);
-    p.setPen(Qt::white);
-    p.drawText(QRect(-20, -20-10, 40, 20), Qt::AlignCenter, QString::number(*handle));
-
-
-    }*/
-    //p.drawText(QRect(value.second*width()/(maximum()-minimum())-20, -16-10, 40, 20), Qt::AlignCenter, QString::number(value.second));
-    //p.drawText(QPoint(value.first*width()/(maximum()-minimum())+12, -5), QString::number(value.first));
 }
