fe0958f68861a5a65d12d25874531a5d5c571334
Bartłomiej Zawalski <bz337736@students.mimuw.edu.pl>
2016-06-03 00:30:08 +0200

    Arranged new dialog widget

--- QChromosome/viewport.cpp
+++ QChromosome/viewport.cpp
@@ -6,6 +6,11 @@ Viewport::Viewport(QWidget *parent) :
     ui(new Ui::Viewport)
 {
     ui->setupUi(this);
+
+    ui->widget->setValue(QColor(Qt::black));
+    ui->widget_2->setValue(QColor(Qt::black));
+    ui->widget_3->setValue(QColor(Qt::black));
+    ui->widget_4->setValue(QColor(Qt::white));
 }
 
 Viewport::~Viewport()
