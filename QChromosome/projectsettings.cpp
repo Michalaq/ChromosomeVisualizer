bdff22c9c67656fff32d99e2f9030a5a14b2995e
Bartłomiej Zawalski <bz337736@students.mimuw.edu.pl>
2017-04-11 00:58:53 +0200

    Finally moved stubs to Preferences

--- QChromosome/projectsettings.cpp
+++ QChromosome/projectsettings.cpp
@@ -1,32 +1,11 @@
 #include "projectsettings.h"
 #include "ui_projectsettings.h"
 
-#include <QSettings>
-#include <QStandardPaths>
-
 ProjectSettings::ProjectSettings(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::Form)
 {
     ui->setupUi(this);
-
-
-
-    /*TODO do usunięcia, kiedy znajdę lepsze miejsce*/
-
-    connect(ui->lineEdit, &QLineEdit::editingFinished, [this]() {
-        QSettings().setValue("povraypath", ui->lineEdit->text());
-    });
-
-    connect(ui->lineEdit_2, &QLineEdit::editingFinished, [this]() {
-        QSettings().setValue("locallib", ui->lineEdit_2->text());
-    });
-
-    QSettings settings;
-    ui->lineEdit->setText(settings.value("povraypath", "/usr/local/share/povray-3.7").toString());
-    ui->lineEdit_2->setText(settings.value("locallib", QStandardPaths::writableLocation(QStandardPaths::HomeLocation)).toString());
-
-    ui->lineEdit_2->setType(FileEdit::FE_Directory);
 }
 
 ProjectSettings::~ProjectSettings()
