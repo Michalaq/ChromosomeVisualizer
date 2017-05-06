a5c66328979903e500d25548aa0e06968cf03ebf
Bartłomiej Zawalski <bz337736@students.mimuw.edu.pl>
2017-05-06 17:42:07 +0200

    Implemented Material::paint

--- QChromosome/material.cpp
+++ QChromosome/material.cpp
@@ -3,7 +3,7 @@
 
 Material::Material(QWidget *parent) : QWidget(parent), clicked(false)
 {
-    setFixedSize(75, 75);
+    setFixedSize(45, 45);
 }
 
 QColor Material::getColor() const
@@ -39,7 +39,26 @@ void Material::setSpecularExponent(qreal e)
 
 void Material::paint(QPainter *painter, QRect bounds)
 {
-    MaterialRenderer::getInstance()->paint(painter, bounds, this);
+    painter->save();
+
+    painter->fillRect(bounds, "#999999");
+
+    painter->setRenderHint(QPainter::Antialiasing);
+
+    painter->translate(bounds.center());
+    painter->rotate(-45.);
+
+    int size = std::min(bounds.width(), bounds.height());
+    qreal step = (qreal)size / 5 / sqrt(2);
+
+    for (int i = -2; i < 3; i++)
+        painter->fillRect(QRectF(-size, 2 * i * step, 2 * size, step), "#666666");
+
+    painter->restore();
+
+    size = size * 5 / 6;
+
+    MaterialRenderer::getInstance()->paint(painter, QRect(bounds.x() + (bounds.width() - size)/2, bounds.y() + (bounds.height() - size)/2, size, size), this);
 }
 
 void Material::mousePressEvent(QMouseEvent *event)
