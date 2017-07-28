b0484a440a27ce103c853ab4bc286b9bfec0adf8
Bartłomiej Zawalski <bz337736@students.mimuw.edu.pl>
2017-07-28 19:03:07 +0200

    Implemented basic functionality of tableModel

--- QChromosome/tablemodel.cpp
+++ QChromosome/tablemodel.cpp
@@ -3,18 +3,13 @@
 TableModel::TableModel(const QStringList &h, QObject *parent) : header(h), QAbstractTableModel(parent)
 {
     //TODO usunąć
-    database.push_back({"0", "LAM", QVariantList()});
-    database.push_back({"1", "BIN", QVariantList()});
+    database.push_back({0, "LAM", QVariantList(), -1});
+    database.push_back({1, "BIN", QVariantList(), -1});
 }
 
 TableModel::~TableModel()
 {
 
-}
-
-Qt::ItemFlags TableModel::flags(const QModelIndex &index) const
-{
-    return QAbstractTableModel::flags(index) | (index.column() == 2 ? Qt::NoItemFlags : Qt::ItemIsEditable);
 }
 
 int TableModel::rowCount(const QModelIndex &parent) const
@@ -33,7 +28,7 @@ QVariant TableModel::data(const QModelIndex &index, int role) const
         return database[index.row()][index.column()];
 
     if (role == Qt::UserRole + 1)
-        return -1;
+        return database[index.row()].last();
 
     return QVariant();
 }
@@ -41,4 +36,30 @@ QVariant TableModel::data(const QModelIndex &index, int role) const
 QVariant TableModel::headerData(int section, Qt::Orientation orientation, int role) const
 {
     return role == Qt::DisplayRole ? header[section] : QVariant();
+}
+
+bool TableModel::setData(const QModelIndex &index, const QVariant &value, int role)
+{
+    if (role == Qt::EditRole)
+        database[index.row()][index.column()] = value;
+
+    emit dataChanged(index, index);
+    return true;
+}
+
+Qt::ItemFlags TableModel::flags(const QModelIndex &index) const
+{
+    return QAbstractTableModel::flags(index) | (index.column() == 2 ? Qt::NoItemFlags : Qt::ItemIsEditable);
+}
+
+bool TableModel::insertRows(int row, int count, const QModelIndex &parent)
+{
+    return true;
+}
+
+bool TableModel::removeRows(int row, int count, const QModelIndex &parent)
+{
+    row += parent.row();
+    database.erase(database.begin() + row, database.begin() + row + count);
+    return true;
 }
