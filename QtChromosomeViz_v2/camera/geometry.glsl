bfacd86ab9c9e5e2fbfe508ac4a586ce5e3b73f1
Bartłomiej Zawalski <bz337736@students.mimuw.edu.pl>
2018-01-29 22:40:19 +0100

    Reimplemented shaders

--- QtChromosomeViz_v2/camera/geometry.glsl
+++ QtChromosomeViz_v2/camera/geometry.glsl
@@ -1,60 +1,72 @@
 #version 330 core
 
 layout (points) in;
-layout (triangle_strip) out;
-layout (max_vertices = 6) out;
+layout (line_strip) out;
+layout (max_vertices = 58) out;
 
 uniform mat4 pro;
 uniform mat4 mv;
 
-flat in vec3 gvInstancePosition[1];
+flat in mat4 gModelview[1];
+flat in mat4 gProjection[1];
 flat in uint giFlags[1];
-flat in uint giInstanceID[1];
-flat in vec4 gcColor[1];
-flat in vec3 gcSpecularColor[1];
-flat in float gfSpecularExponent[1];
-flat in float gfInstanceSize[1];
 
-out vec4 vPosition;
-out vec3 vViewPosition;
-
-flat out vec3 vInstancePosition;
-flat out uint iFlags;
-flat out uint iInstanceID;
-flat out vec4 cColor;
-flat out vec3 cSpecularColor;
-flat out float fSpecularExponent;
-flat out float fInstanceSize;
-
-// this can be passed from CPU (as uniform?) 
-vec3 tetrahedron[4] = vec3[](
-    vec3(+1, +1, +1),
-    vec3(-1, -1, +1),
-    vec3(-1, +1, -1),
-    vec3(+1, -1, -1)
+vec3 tab[] = vec3[](
+    vec3(+10.625, -10.625, 0), vec3(+10.625, +10.625, 0), vec3(-10.625, +10.625, 0), vec3(-10.625, -10.625, 0),
+    vec3(+3.75, -3.75, 12.5), vec3(+3.75, +3.75, 12.5), vec3(-3.75, +3.75, 12.5), vec3(-3.75, -3.75, 12.5),
+    vec3(+12.5, -12.5, 12.5), vec3(+12.5, +12.5, 12.5), vec3(-12.5, +12.5, 12.5), vec3(-12.5, -12.5, 12.5),
+    vec3(+12.5, -12.5, 62.5), vec3(+12.5, +12.5, 62.5), vec3(-12.5, +12.5, 62.5), vec3(-12.5, -12.5, 62.5),
+    vec3(0, 25, 25), vec3(0, 25, 50)
 );
+
+const float PI = 3.1415926535897932384626433832795;
 
 void main() {
     if ((giFlags[0] & 2u) == 2u)
     {
-        vec4 objectSpacePos = mv * vec4(gvInstancePosition[0], 1.0);
-        
-        vInstancePosition = objectSpacePos.xyz / objectSpacePos.w;
-        iFlags = giFlags[0];
-        iInstanceID = giInstanceID[0];
-        cColor = gcColor[0];
-        cSpecularColor = gcSpecularColor[0];
-        fSpecularExponent = gfSpecularExponent[0];
-        fInstanceSize = gfInstanceSize[0];
-        
-        for(int i = 0; i < 6; i++)
-        {
-            vViewPosition = vInstancePosition + tetrahedron[i & 3] * fInstanceSize * 3.5;
-            gl_Position = vPosition = pro * vec4(vViewPosition, 1.0);
-            
-            EmitVertex();
-        }
-        
+        gl_Position = pro * mv * vec4(tab[0], 1); EmitVertex();
+        gl_Position = pro * mv * vec4(tab[1], 1); EmitVertex();
+        gl_Position = pro * mv * vec4(tab[2], 1); EmitVertex();
+        gl_Position = pro * mv * vec4(tab[3], 1); EmitVertex();
+        gl_Position = pro * mv * vec4(tab[0], 1); EmitVertex();
+        gl_Position = pro * mv * vec4(tab[4], 1); EmitVertex();
+        gl_Position = pro * mv * vec4(tab[5], 1); EmitVertex();
+        gl_Position = pro * mv * vec4(tab[6], 1); EmitVertex();
+        gl_Position = pro * mv * vec4(tab[7], 1); EmitVertex();
+        gl_Position = pro * mv * vec4(tab[4], 1); EmitVertex();
+        EndPrimitive();
+        gl_Position = pro * mv * vec4(tab[1], 1); EmitVertex();
+        gl_Position = pro * mv * vec4(tab[5], 1); EmitVertex();
+        EndPrimitive();
+        gl_Position = pro * mv * vec4(tab[2], 1); EmitVertex();
+        gl_Position = pro * mv * vec4(tab[6], 1); EmitVertex();
+        EndPrimitive();
+        gl_Position = pro * mv * vec4(tab[3], 1); EmitVertex();
+        gl_Position = pro * mv * vec4(tab[7], 1); EmitVertex();
+        EndPrimitive();
+        gl_Position = pro * mv * vec4(tab[8], 1); EmitVertex();
+        gl_Position = pro * mv * vec4(tab[9], 1); EmitVertex();
+        gl_Position = pro * mv * vec4(tab[10], 1); EmitVertex();
+        gl_Position = pro * mv * vec4(tab[11], 1); EmitVertex();
+        gl_Position = pro * mv * vec4(tab[8], 1); EmitVertex();
+        gl_Position = pro * mv * vec4(tab[12], 1); EmitVertex();
+        gl_Position = pro * mv * vec4(tab[13], 1); EmitVertex();
+        gl_Position = pro * mv * vec4(tab[14], 1); EmitVertex();
+        gl_Position = pro * mv * vec4(tab[15], 1); EmitVertex();
+        gl_Position = pro * mv * vec4(tab[12], 1); EmitVertex();
+        EndPrimitive();
+        gl_Position = pro * mv * vec4(tab[9], 1); EmitVertex();
+        gl_Position = pro * mv * vec4(tab[13], 1); EmitVertex();
+        EndPrimitive();
+        gl_Position = pro * mv * vec4(tab[10], 1); EmitVertex();
+        gl_Position = pro * mv * vec4(tab[14], 1); EmitVertex();
+        EndPrimitive();
+        gl_Position = pro * mv * vec4(tab[11], 1); EmitVertex();
+        gl_Position = pro * mv * vec4(tab[15], 1); EmitVertex();
+        EndPrimitive();
+        for (int i = 0; i < 13; i++) {gl_Position = pro * mv * vec4(tab[16] + 12.5 * vec3(0, cos(PI/6*i), sin(PI/6*i)), 1); EmitVertex();}
+        EndPrimitive();
+        for (int i = 0; i < 13; i++) {gl_Position = pro * mv * vec4(tab[17] + 12.5 * vec3(0, cos(PI/6*i), sin(PI/6*i)), 1); EmitVertex();}
         EndPrimitive();
     }
 }
--- QtChromosomeViz_v2/camera/vertex.glsl
+++ QtChromosomeViz_v2/camera/vertex.glsl
@@ -3,20 +3,12 @@
 layout(location = 0) in mat4 modelview;
 layout(location = 1) in mat4 projection;
 
-flat out vec3 gvInstancePosition;
+flat out mat4 gModelview;
+flat out mat4 gProjection;
 flat out uint giFlags;
-flat out uint giInstanceID;
-flat out vec4 gcColor;
-flat out vec3 gcSpecularColor;
-flat out float gfSpecularExponent;
-flat out float gfInstanceSize;
 
 void main() {
-    gvInstancePosition = vec3(0, 0, 0);
+    gModelview = modelview;
+    gProjection = projection;
     giFlags = 2u;
-    giInstanceID = 0u;
-    gcColor = vec4(1,0,0,1);
-    gcSpecularColor = vec3(1,1,1);
-    gfSpecularExponent = 0.4;
-    gfInstanceSize = 3;
 }
