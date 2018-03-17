c7ceb7ffe9f6df069595b156e782553712efde32
Bartłomiej Zawalski <bz337736@students.mimuw.edu.pl>
2018-03-18 00:42:37 +0100

    [bugfix] Generating config.h on compilation

--- install.sh
+++ install.sh
@@ -3,7 +3,13 @@ DESTDIR=build-QChromosome-Desktop_Qt_5_9_3_GCC_64bit-Debug
 
 protoc --proto_path=QtChromosomeViz_v2/bartekm_code/message_format --cpp_out=QtChromosomeViz_v2/bartekm_code/message_format QtChromosomeViz_v2/bartekm_code/message_format/message.proto
 
+mkdir tmp
+cd tmp
+cmake ..
+cd ..
+rm -rf tmp
+
 mkdir -p $DESTDIR
 cd $DESTDIR
-$QTDIR/bin/qmake ../QChromosome/QChromosome.pro -spec linux-g++ CONFIG+=debug CONFIG+=qml_debug
+$QTDIR/bin/qmake ../QChromosome/QChromosome.pro CONFIG+=debug CONFIG+=qml_debug
 /usr/bin/make
